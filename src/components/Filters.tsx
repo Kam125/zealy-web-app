c6faafef501d08abdf9fb6d9e806897bcdbdd3b9
dawoodkhalid333 <136672639+dawoodkhalid333@users.noreply.github.com>
2023-08-04 12:15:10 +0500

    warnings removed

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -23,67 +23,66 @@ function Filters() {
 
   return (
     <Row className='filters'>
-      <Col lg='4' md='12' className='d-flex gap-2 mb-4'>
-      <Button className='d-flex align-items-center gap-2'>
-      <i className="fa fa-asterisk"></i>
-        <span>New</span>
-      </Button>
-      <Button className='d-flex align-items-center gap-2'>
-      <i className="fa-solid fa-fire"></i>
-        <span>New</span>
-      </Button>
-      <Button className='d-flex align-items-center gap-2'>
-      <i className="fa fa-asterisk"></i>
-        <span>Trendy</span>
-      </Button>
-      <Button className='d-flex align-items-center gap-2'>
-      <i className="fa fa-asterisk"></i>
-        <span>New</span>
-      </Button>
-      </Col> 
-      <Col className='right-filters-container'>
-        <div className='scroll-container' ref={filtersContainerRef}>
-      <Col className='d-flex gap-2 rigt-filters'>
-      <Button className='d-flex align-items-center gap-2'>     
-        <span>All</span>
-      </Button>
-      <Button className='d-flex align-items-center gap-2'>
-        <span>Startup</span>
-      </Button>
-      <Button className='d-flex align-items-center gap-2'>
-        <span>Gaming</span>
-      </Button>
-      <Button className='d-flex align-items-center gap-2'>
-        <span>Infrastructure</span>
-      </Button>
-      <Button className='d-flex align-items-center gap-2'>
-        <span>Startup</span>
-      </Button>
-      <Button className='d-flex align-items-center gap-2'>
-        <span>Gaming</span>
-      </Button>
-      <Button className='d-flex align-items-center gap-2'>
-        <span>Infrastructure</span>
-      </Button>
-      <Button className='d-flex align-items-center gap-2'>
-        <span>Startup</span>
-      </Button>
-      <Button className='d-flex align-items-center gap-2'>
-        <span>Gaming</span>
-      </Button>
-      <Button className='d-flex align-items-center gap-2'>
-        <span>Infrastructure</span>
-      </Button>
-      <Button className='scroll-button left-scroll-button' onClick={handleScrollLeft}>
-          Left
+      <Col lg='4' md='12' className='d-flex gap-2 mb-4 left-filters-container'>
+        <Button className='d-flex align-items-center gap-2'>
+          <i className="fa fa-asterisk"></i>
+          <span>New</span>
         </Button>
-        <Button className='scroll-button right-scroll-button' onClick={handleScrollRight}>
-          Right
-        </Button> 
-      </Col> 
-      </div>
-      </Col> 
-     
+        <Button className='d-flex align-items-center gap-2'>
+          <i className="fa-solid fa-fire"></i>
+          <span>New</span>
+        </Button>
+        <Button className='d-flex align-items-center gap-2'>
+          <i className="fa fa-asterisk"></i>
+          <span>Trendy</span>
+        </Button>
+        <Button className='d-flex align-items-center gap-2'>
+          <i className="fa fa-asterisk"></i>
+          <span>New</span>
+        </Button>
+      </Col>
+      <Col lg='8' md='12' className='right-filters-container'>
+        <div className='scroll-container' ref={filtersContainerRef}>
+          <Col className='d-flex gap-2 rigt-filters'>
+            <Button className='d-flex align-items-center gap-2'>
+              <span>All</span>
+            </Button>
+            <Button className='d-flex align-items-center gap-2'>
+              <span>Startup</span>
+            </Button>
+            <Button className='d-flex align-items-center gap-2'>
+              <span>Gaming</span>
+            </Button>
+            <Button className='d-flex align-items-center gap-2'>
+              <span>Infrastructure</span>
+            </Button>
+            <Button className='d-flex align-items-center gap-2'>
+              <span>Startup</span>
+            </Button>
+            <Button className='d-flex align-items-center gap-2'>
+              <span>Gaming</span>
+            </Button>
+            <Button className='d-flex align-items-center gap-2'>
+              <span>Infrastructure</span>
+            </Button>
+            <Button className='d-flex align-items-center gap-2'>
+              <span>Startup</span>
+            </Button>
+            <Button className='d-flex align-items-center gap-2'>
+              <span>Gaming</span>
+            </Button>
+            <Button className='d-flex align-items-center gap-2'>
+              <span>Infrastructure</span>
+            </Button>
+            <Button className='scroll-button left-scroll-button' onClick={handleScrollLeft}>
+              ←
+            </Button>
+            <Button className='scroll-button right-scroll-button' onClick={handleScrollRight}>
+              →
+            </Button>
+          </Col>
+        </div>
+      </Col>
     </Row>
   )
 }
